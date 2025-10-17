811b17acb07c6da679fe56cc30249c24ed8ca600
ken204 <Kenny20041223@gmail.com>
2025-10-17 15:22:41 +0800

    Initial Commit

---
+++ 01257057HW3/_1257057HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  _1257057HW3App.swift
+//  01257057HW3
+//
+//  Created by user05 on 2025/10/17.
+//
+
+import SwiftUI
+
+@main
+struct _1257057HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
